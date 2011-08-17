f6b20b510ff94a12bed1f961bb6f75b0f8dd6fe3
Demid Lupin <lupin@demid.su>
2011-08-17 19:19:26 +0400

    Fixed wrong commit

--- app/models/source_issue_relation.rb
+++ app/models/source_issue_relation.rb
@@ -4,17 +4,6 @@ class SourceIssueRelation < ActiveRecord::Base
 
   belongs_to :issue_from, :class_name => 'SourceIssue', :foreign_key => 'issue_from_id'
   belongs_to :issue_to, :class_name => 'SourceIssue', :foreign_key => 'issue_to_id'
-  
-  def self.migrate
-    all.each do |source_issue_relation|
-
-      IssueRelation.create!(source_issue_relation.attributes) do |ir|
-       class SourceIssueRelation < ActiveRecord::Base
-  include SecondDatabase
-  set_table_name :issue_relations
-
-  belongs_to :issue_from, :class_name => 'SourceIssue', :foreign_key => 'issue_from_id'
-  belongs_to :issue_to, :class_name => 'SourceIssue', :foreign_key => 'issue_to_id'
 
   def self.migrate
     all.each do |source_issue_relation|
@@ -28,7 +17,3 @@ class SourceIssueRelation < ActiveRecord::Base
 end
 
 
-      end
-    end
-  end
-end
