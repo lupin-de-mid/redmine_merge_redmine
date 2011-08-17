c9a21314d58ce8e9a58f6b21e5845edd57b781ee
Demid Lupin <lupin@demid.su>
2011-08-17 19:16:56 +0400

    Fixed issue relation  merging Search by new id but not by subject

--- app/models/source_issue_relation.rb
+++ app/models/source_issue_relation.rb
@@ -9,8 +9,25 @@ class SourceIssueRelation < ActiveRecord::Base
     all.each do |source_issue_relation|
 
       IssueRelation.create!(source_issue_relation.attributes) do |ir|
-        ir.issue_from = Issue.find_by_subject(source_issue_relation.issue_from.subject)
-        ir.issue_to = Issue.find_by_subject(source_issue_relation.issue_to.subject)
+       class SourceIssueRelation < ActiveRecord::Base
+  include SecondDatabase
+  set_table_name :issue_relations
+
+  belongs_to :issue_from, :class_name => 'SourceIssue', :foreign_key => 'issue_from_id'
+  belongs_to :issue_to, :class_name => 'SourceIssue', :foreign_key => 'issue_to_id'
+
+  def self.migrate
+    all.each do |source_issue_relation|
+
+      IssueRelation.create!(source_issue_relation.attributes) do |ir|
+        ir.issue_from = Issue.find RedmineMerge::Mapper.get_new_issue_id(source_issue_relation.issue_from.id)
+        ir.issue_to = Issue.find RedmineMerge::Mapper.get_new_issue_id(source_issue_relation.issue_to.id)
+      end
+    end
+  end
+end
+
+
       end
     end
   end
